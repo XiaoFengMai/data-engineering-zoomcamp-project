c4c2752af4b16171defa29356466672d67d70940
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-06 13:37:35 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,7 @@
 resource "google_storage_bucket" "data_lake_bucket" {
   name          = var.gcs_bucket_name
   location      = var.region
+  storage_class = "STANDARD"
   force_destroy = true
 
   uniform_bucket_level_access = true
