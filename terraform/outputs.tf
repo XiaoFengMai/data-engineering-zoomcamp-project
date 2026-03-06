42e1dff3fab3bd39e21bbbb2d04fbb3fd32a990b
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-06 12:23:45 -0500

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,17 @@
+# print values after deployment
 
+
+output "bucket_name" {
+  value = google_storage_bucket.data_lake_bucket.name
+}
+
+output "bigquery_dataset" {
+  value = google_bigquery_dataset.dataset.dataset_id
+}
+
+
+
+# after running terraform, you should see
+
+#bucket_name = nyc-311-data-lake
+#bigquery_dataset = nyc_311_dataset
