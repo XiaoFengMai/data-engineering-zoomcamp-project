5c0439b268c09344d1945375dbb2447c56363fcc
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-06 16:07:59 -0500

    Update nyc_311_flow.py

--- flows/nyc_311_flow.py
+++ flows/nyc_311_flow.py
@@ -1,1 +1,15 @@
+from prefect import flow, task
+import subprocess
 
+@task
+def run_dlt_pipeline():
+  subprocess.run(["python", "pipelines/nyc_311_pipeline.py"], check=True)
+
+@flow(name="nyc_311_batch_pipeline")
+def nyc_311_flow():
+
+  run_dlt_pipeline()
+
+
+if __name__ == "__main__":
+  nyc_311_flow()
