8a3b68371066162e971a8c5b96c6d4924e6d7bc6
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-06 12:21:52 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,25 @@
+# create data lake bucket in google cloud storage
+
+resource "google_storage_bucket" "data_lake_bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.region
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+}
+
+
+
+
+# create data warehouse dataset in bigquery
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id  = var.bq_dataset_name
+  location    = var.region
+  description = "NYC 311 analytics dataset"
+}
 
