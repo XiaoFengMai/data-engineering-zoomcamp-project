39ee30b613bc2492b27b8c81593f01999c211e01
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-09 13:53:19 -0400

    Update nyc_311_prefect_flow.py

--- workflow_orchestration/nyc_311_prefect_flow.py
+++ workflow_orchestration/nyc_311_prefect_flow.py
@@ -1,1 +1,20 @@
 
+# prefect flow
+from prefect import flow, task
+import subprocess
+
+@task
+def run_ingestion():
+    subprocess.run(["python", "ingestion_pipeline/nyc_311_pipeline.py"], check=True)
+
+@task
+def run_dbt():
+    subprocess.run(["dbt", "run"], cwd="data_build_tool/", check=True)
+
+@flow
+def nyc_pipeline():
+    ingestion = run_ingestion()
+    run_dbt(wait_for=[ingestion])
+
+if __name__ == "__main__":
+    nyc_pipeline()
