5a3efdbf61206c00a378522a2c6101b4f7e5ec44
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-09 16:34:46 -0400

    Update stg_311_requests.sql

--- data build tool/models/staging/stg_311_requests.sql
+++ data build tool/models/staging/stg_311_requests.sql
@@ -1,13 +1,14 @@
 
 SELECT
-    unique_key,
-    created_date,
-    closed_date,
-    complaint_type,
-    descriptor,
-    borough,
-    agency,
-    status,
-    latitude,
+    unique_key,            # uniquely identify 311 requests; prevent duplicates
+    created_date,            # trend analysis, partioning
+    closed_date,            # compute resolution time
+    complaint_type,            # categorize complaints
+    descriptor,            # more detailed complaint description
+    borough,            # request location: manhattan, brooklyn, queens, staten island, bronx
+    agency,            # which city department
+    status,            
+    latitude,           # mapping visualizations
     longitude
 FROM `YOUR_GCP_PROJECT_ID.nyc_311_raw.service_requests`        # must edit GCP project id here
+                                                                # selects the raw dataset that the pipeline loaded earlier
