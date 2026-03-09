0964460b6cc2a5cdf3fa7739c3632f51b1b6c15a
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-09 13:34:05 -0400

    Update complaints_by_type.sql

--- data build tool/models/marts/complaints_by_type.sql
+++ data build tool/models/marts/complaints_by_type.sql
@@ -2,7 +2,7 @@
 SELECT
     complaint_type,
     COUNT(*) AS total_requests
-FROM {{ ref('stg_311_requests') }}
+FROM {{ ref('stg_311_cleaned') }}
 GROUP BY complaint_type
 ORDER BY total_requests DESC
 
