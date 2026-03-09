f3d2df76ae51edbe40ca25562711410c1300e6ee
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-09 12:18:03 -0400

    Update stg_311_requests.sql

--- data build tool/models/staging/stg_311_requests.sql
+++ data build tool/models/staging/stg_311_requests.sql
@@ -2,11 +2,12 @@
 SELECT
     unique_key,
     created_date,
+    closed_date,
     complaint_type,
-    address_type,
+    descriptor,
     borough,
     agency,
     status,
     latitude,
     longitude
-FROM {{ source('nyc_311_raw', 'nyc_311_requests') }}
+FROM `YOUR_GCP_PROJECT_ID.nyc_311_raw.service_requests`        # must edit GCP project id here
