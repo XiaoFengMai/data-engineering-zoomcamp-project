437756d7e8e573e4a5340ff9ec2cb552fc09077d
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-10 10:34:46 -0400

    Update complaints_by_type.sql

--- data_build_tool/models/marts/complaints_by_type.sql
+++ data_build_tool/models/marts/complaints_by_type.sql
@@ -1,7 +1,9 @@
 # this query filters complaint types displayed within looker dashboard charts
-SELECT
-    complaint_type,
-    COUNT(*) AS total_requests
-FROM {{ ref('stg_311_cleaned') }}
-GROUP BY complaint_type
-ORDER BY total_requests DESC
+    
+SELECT                    # starts aggregation query
+    complaint_type,                   # groups data by complaint type
+    COUNT(*) AS total_requests            # counts number of service requests
+FROM {{ ref('int_311_cleaned') }}           # uses cleaned intermdiate dataset     
+GROUP BY complaint_type                # groups records by complaint category
+ORDER BY total_requests DESC                # sorts complaint results from most common to least common
+                                            # ideal for dashboard bar charts
