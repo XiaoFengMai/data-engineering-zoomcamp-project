bbdf049d014a7259dcfdf0f3392c51d92847d92c
XiaoFengMai <113124825+XiaoFengMai@users.noreply.github.com>
2026-03-10 16:37:41 -0400

    Update fct_311_complaints.sql

--- data_build_tool/models/marts/fct_311_complaints.sql
+++ data_build_tool/models/marts/fct_311_complaints.sql
@@ -6,5 +6,3 @@ SELECT                # initiates query
     COUNT(*) AS complaint_count                # counts every row that has borough and complaints data and labels it complaint_count (*) -> regardless of null values
 FROM {{ ref('stg_311_requests') }}        # pulls data from stg_311_requests staging model using dbt's ref() function
 GROUP BY borough, complaint_type               # creates a row for every unique combinaition of borough and complaint type 
-
-# 
